53f11c6c530458487f56ea84741a49ffb9a67a04
ChandlerFerry <chandlerferry@ferrysystems.io>
2023-03-19 03:52:14 -0700

    chore: enable tests

--- src/p3d/verification.test.ts
+++ src/p3d/verification.test.ts
@@ -1,6 +1,6 @@
 import { verifyOnChainIdentity } from './verification';
 describe('verifyOnChainIdentity', () => {
-	xit('should pass with completed wallet', async () => {
+	it('should pass with completed wallet', async () => {
 		const result = await verifyOnChainIdentity(
 			'd1J1WymQy1aVqstxWdY7wE6V1RNFtHkK68g3KKW1Sc3rUmBVF',
 			'Chandler#9999',
@@ -24,7 +24,7 @@ describe('verifyOnChainIdentity', () => {
 		});
 	});
 
-	xit('should fail if discord name mismatch', async () => {
+	it('should fail if discord name mismatch', async () => {
 		const result = await verifyOnChainIdentity(
 			'd1J1WymQy1aVqstxWdY7wE6V1RNFtHkK68g3KKW1Sc3rUmBVF',
 			'Bob#1234',
